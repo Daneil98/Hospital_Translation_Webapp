3c7ff94f77746fe9f140650a64da8af925eb180f
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2025-03-03 00:37:05 +0100

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from socket import fromshare
 from django.forms.widgets import DateInput
 from django.utils.translation import gettext_lazy as _
 from .models import *
