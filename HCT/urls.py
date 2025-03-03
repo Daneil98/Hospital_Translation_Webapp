a8137d1700c3e35daebc99c628b905b687800fc9
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2025-03-03 11:02:48 +0100

    Update urls.py

--- HCT/urls.py
+++ HCT/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from account import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name ='index'),
-    path('account/', include('account.urls'))
+    path('', include('account.urls'))
 ]
 
 if settings.DEBUG:
