67285125469fc50fc75b7e8e09681e08f75c3854
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2025-03-03 10:36:02 +0100

    Update urls.py

--- HCT/urls.py
+++ HCT/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.transcribe, name ='transcription'),
+    path('', views.index, name ='index'),
     path('account/', include('account.urls'))
 ]
 
